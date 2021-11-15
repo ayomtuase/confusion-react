61277e787d04bf238c1d92ac53a5ca8cd49145f6
Ayomide Oguntuase <oguntuase3@gmail.com>
2021-11-15 14:16:10 +0100

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,7 +4,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, ListGroup, ListGroupItem 
 import { Link } from 'react-router-dom';
 import {
     Breadcrumb, BreadcrumbItem, Button, Modal, ModalBody,
-    Label, ModalHeader, Row, Col,
+    Label, ModalHeader
 } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
@@ -25,7 +25,7 @@ function RenderDish({ dish }) {
     }
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId}) {
     if (comments != null) {
         let commentsList = (comments.map((comment) => {
             return (<ListGroup key={comment.id}>
@@ -38,7 +38,7 @@ function RenderComments({ comments }) {
             <div>
                 <h4>Comments</h4>
                 {commentsList}
-                <CommentForm/>
+                <CommentForm addComment={addComment} dishId={dishId}/>
             </div>
         );
 
@@ -71,8 +71,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);        
     }
 
 
@@ -149,8 +148,7 @@ const DishDetail = (props) => {
                     <RenderDish dish={props.dish} />
                 </div>
                 <div className="col-12 col-md-5 m-1">
-                    <RenderComments comments={props.comments} />
-                    
+                    <RenderComments comments={props.comments} addComment={props.addComment} dishId={props.dish.id}/>                    
                 </div>
             </div>
 
